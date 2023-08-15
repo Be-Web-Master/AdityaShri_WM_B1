960af319856e92d22635ce057a992bcbafcba6f3
Aditya_Shrivastava <94391248+shrivastava6781@users.noreply.github.com>
2023-08-15 19:34:41 +0530

    Strings Assignment Completed

---
+++ script.js
@@ -0,0 +1,128 @@
+// Assignment Questions of String from webMaster
+
+
+
+// const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+// 1 --  console.log(sentence[6]);
+// 2 --  const character = sentence[sentence.length - 4];
+// 3 --  const character = sentence.charCodeAt('f');
+// console.log(character)
+
+
+// 4 ---
+// const firstName = "Aditya";
+// const lastName = "Shrivastva";
+// const fullName = firstName.concat(" ",lastName);
+// console.log(fullName);
+
+
+// 5 ----
+// var a = "Namaste world ram ram bhiyo";
+// var b = "ram ram bhiyo";
+
+// var c = a.endsWith(b)
+// console.log(c);
+
+
+// 6----
+// const asciiCodes = [76,74,79,65,75];
+
+// const greeting = String.fromCharCode(...asciiCodes);
+
+// console.log(greeting);
+
+// 7 ---
+// const a = "😀"
+// const ans = a.codePointAt(0);
+// console.log(ans);
+ 
+
+// 8 ---
+// const message = "The code is not working. The code is causing errors.";
+// const ans = message.replaceAll("code", "program");
+// console.log(ans);
+
+// 9---
+// const message =  "I like to eat bananas. Bananas are a healthy snack.";
+// const ans = message.replace("bananas", "apples");
+// console.log(ans);
+
+
+// 10----
+// const str = "red,green,blue,yellow";
+// const ans = str.split(',');
+// console.log(ans);
+
+
+// 11 -----
+// const address = "1234 Elm Street, Citysville";
+// const ans = address.substring(6)
+
+// console.log(ans);
+
+// 12 ----
+// const address = "JavaScript is a powerful and flexible language.";
+// const ans = address.substring(11,26);
+
+// console.log(ans);
+
+// 13-----
+// const sentence = "I enjoy programming in JavaScript."
+// const ans = sentence.includes("JavaScript");
+// console.log(ans);
+
+// 14-----
+// const fruit = "apple, banana, orange, mango, grape";
+// const ans = fruit.includes("orange" , 18);
+// console.log(ans);
+
+
+// 15-----
+// const str = " Hello, world! ";
+// const ans = str.trim(" ");
+// console.log(ans);
+
+// 16-----
+// const str = "elephant";
+// const ans = str.charAt(2);
+// console.log(ans);
+
+// 17--- 
+// const str = [72,101,108,108,111];
+// const ans = String.fromCharCode(...str);
+// console.log(ans);
+
+
+// 18----
+// const str = [128512,128513,128514];
+// const ans = String.fromCharCode(...str);
+// console.log(ans);
+
+
+// 19-----
+// const str = "The sun is shining. The weather is sunny.";
+// const ans = str.match("sun");
+// console.log(ans);
+
+
+// 20----
+// const str = "Apples are delicious. Bananas are tasty. Grapes are sweet.";
+// const ans = str.matchAll("es");
+// console.log(ans);
+
+// 21-----
+// const str = "Hello, how are you doing?";
+// const ans = str.startsWith("Hello");
+// console.log(ans);
+
+// 22-----
+// const str = "The quick brown fox jumps over the lazy dog.";
+// const ans = str.indexOf("fox");
+// console.log(ans);
+
+
+
+
+
+

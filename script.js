3dbffe13a0c0d155898ccf1e21771bd2bf8f150e
Aditya_Shrivastava <94391248+shrivastava6781@users.noreply.github.com>
2023-08-23 11:28:00 +0530

    Dom Question incomplete

---
+++ script.js
@@ -0,0 +1,230 @@
+// 1------
+ 
+// var id = document.getElementById("name");
+// console.log(id)
+
+// 2----
+
+// var elem = document.getElementById("name")
+// console.log(elem.type)
+
+// 3-----
+
+// var elem = document.getElementById("name")
+// console.log(elem.name)
+
+// 4----
+// var elem = document.getElementById("name")
+// console.log(elem.id)
+ 
+
+// 5----
+// var elem = document.getElementById("name")
+// console.log(elem.value)
+
+// 6----
+// var elem = document.querySelectorAll("li")
+// console.log(elem)
+
+// 7-----
+// var elem = document.getElementById("name")
+// console.log(elem.id)
+
+// 8----
+//  var a = document.body.children;
+//  console.log(a)
+
+// 9-----   puchna padega
+// var a = document.getElementsByTagName("li");
+//  console.log(a)
+
+
+// 10------
+// var a = document.getElementsByClassName("content")
+// console.log(a)
+
+
+// 11-----
+// var a = document.getElementsByName("fullName")
+// console.log(a)
+
+// 12-----
+// var a = document.getElementById("main-heading")
+// console.log(a.classList)
+
+
+// 13-----
+// var a = document.getElementById("main-heading")
+// console.log(a.classList.add("highlight"));
+
+
+// 14-----
+// var a = document.getElementById("main-heading")
+// console.log(a.classList.remove("highlight"))
+
+
+// 15-----
+// var a = document.getElementById("container")
+// console.log(a.classList.toggle("active","innerbox"))
+
+
+// 16-----
+// var a = document.getElementById("name")
+// console.log(a.classList.contains("input-field"))
+
+// 17---
+// var a = document.getElementById("main-heading")
+// a.style.backgroundColor = "green";
+
+
+// 18----   ye puchna he
+// var a = document.querySelector(".list");
+// var b = a.querySelector("li");
+// var c = b.textContent === "Item 2";
+// console.log(c);
+
+// 19-----  
+// var a = document.querySelector(".content")
+// var b = a === "div";
+// console.log(b)
+
+// 20------
+// var a = document.querySelector(".inner-box")
+// var b = a.closest("#container");
+// console.log(b)
+
+
+// 21-----
+// var a = document.body.firstElementChild;
+// console.log(a)
+
+
+// 22-----
+// var a = document.body;
+// console.log(a.children)
+
+
+// 23----   ye nhi pata 
+// var a = document.body.firstChild;
+// console.log(a.tagName)
+
+
+// 24----.      ye nhi pata 
+// var a = document.body.firstChild.className;
+// console.log(a);
+
+
+// 25-----
+// var a = document.querySelector(".content")
+// console.log(a.innerHTML)
+
+// 26-----
+// var a = document.querySelector(".content")
+// console.log(a.innerText)
+
+
+
+// 27-----
+// var a = document.getElementById("name")
+// console.log(a.getAttribute("type"));
+
+
+// 28------   
+// var a = document.querySelector("#name")
+// a.setAttribute("placholder","Enter Your Full Name");
+
+
+// 29-----
+// var a = document.querySelector("#name")
+// console.log(a.hasAttribute("placeholder"))
+
+
+// 30------
+// var a = document.querySelector("#name")
+// console.log(a.removeAttribute("value"))
+
+// 31-----   ek barr ye bhi 
+// var a = document.getElementById("main-heading")
+// console.log(a.attributes);
+
+
+// 32----
+// var a = document.createElement("div")
+// console.log(a);
+
+// 33---
+// var a = document.createTextNode("This is New text node")
+// console.log(a);
+
+// 34----  ye dekhna he 
+// var a = document.getElementById("main-heading")
+// console.log(a.append)
+
+// 35----   ye bhi
+// var a = document.querySelector(".content")
+// console.log(a.prepend("div"))
+
+
+// 36---- 
+// var a = document.getElementById("main-heading")
+// console.log(a.replaceWith("Text node"))
+
+// 37------  ye bhi nhi aa rhaa
+// var a = document.querySelector(".inner-box")
+// console.log(a.before("div"))
+
+
+// 38------- ye bhi nhi pata 
+
+
+// 39------
+// var a = document.querySelector(".content");
+// console.log(a.offsetHeight);
+// console.log(a.offsetWidth);
+
+
+// 40-------
+// var a = document.querySelector(".content");
+// console.log(a.clientHeight);
+// console.log(a.clientWidth);
+
+// 41-------
+// var a = document.querySelector(".content");
+// console.log(a.scrollHeight);
+// console.log(a.scrollWidth);
+
+
+
+// 42-------
+// console.log(window.innerHeight);
+// console.log(window.innerWidth);
+
+
+
+// 43-------
+// console.log(window.outerHeight);
+// console.log(window.outerWidth);
+
+// 44--------  ye ek baar check krna padega matlab shi he lekin fir bhi 
+// var a = document.querySelector("#main-heading");
+// a.innerHTML = "Aditya Shrivastava";
+
+
+// 45-------
+// var a = document.querySelector("#name");
+// var b = a.value = "Jane Smith";
+// console.log(b)
+
+// 46-------  
+// var a = document.querySelector(".list");
+// var b = document.createElement("li");
+// b.textContent = "Item 4";
+// a.appendChild(b);
+// console.log(a)
+// console.log(b)
+
+
+
+
+
+ 

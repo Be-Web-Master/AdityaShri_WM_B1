e1b3edb75efebb6007761bd07df2cecab9595e99
shrivastava6781 <adityashrivastava6781@gmail.com>
2023-09-12 00:22:09 +0530

    todo with different parts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react"
 
-function App() {
+// get localStorage data 
+
+const getLocalData = () =>{
+  let list = localStorage.getItem("Task")
+  console.log(list)
+  if(list)
+  {
+    return JSON.parse(localStorage.getItem("Task"));
+  }
+  else{
+    return [];
+  }
+} 
+function Task() {
+  const [input, setInput] = useState("")
+  const [desc, setDesc] = useState("")
+  const [data, setdata] = useState(getLocalData())
+
+
+
+  const submitHandler = (e) =>{
+    e.preventDefault()
+    console.log(input)
+    // const newData = [data,...input]
+    const newData = ([...data,{input,desc}])
+    setdata(newData)
+    setInput("")
+    setDesc("")
+    console.log(data)
+  }
+  let showList = <h2>No task</h2>
+  if(data.length > 0){
+    showList = data.map((obj ,idx) =>{
+      return <li id='card' key={idx}>{obj.input}<span> {obj.desc} </span><button onClick = {() => {
+        let copy = [...data]
+        copy.splice(idx,1)
+        setdata(copy)
+      }}>X</button></li>
+    })
+  }
+
+  // add data to Local Storage 
+  useEffect(() => {
+    localStorage.setItem("Task",JSON.stringify(data))
+  }, [data])
+  
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+    <form  onSubmit={submitHandler}>
+      <input type="text" id='input' placeholder="Enter the Title" value={input} onChange={(e) =>{
+        setInput(e.target.value)
+      }}/>
+      <input type="text" id='desc' placeholder="Enter the Desc" value={desc} onChange={(e) =>{
+        setDesc(e.target.value)
+      }}/>
+      <button id='submitbtn' type="submit">submit</button>
+    </form>
+    <hr/>
+
+    <div>
+      <ol>
+        {showList}
+      </ol>
     </div>
+    
+    </>
   );
 }
 
-export default App;
+export default Task;

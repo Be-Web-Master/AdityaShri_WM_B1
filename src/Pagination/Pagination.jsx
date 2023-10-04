1cfea8154a3e4cfcd61126fc2458e6f4a6de0e9b
shrivastava6781 <adityashrivastava6781@gmail.com>
2023-10-04 17:07:26 +0530

    project incompleted

---
+++ src/Pagination/Pagination.jsx
@@ -0,0 +1,39 @@
+import React,{useEffect , useState} from "react"
+import "./Pagination.css"
+const Pagination = ({data=[],pageSize = 5,onchange =() =>{ }, pageOptions = 5}) =>{
+    const [currentPage , setCurrentPage] = useState(1);
+    const totalPages = Math.ceil(data.length / pageSize)
+    const handleOnChange = (pageNo) =>{
+        console.log({pageNo,})
+        onchange (data.slice((pageNo - 1) * pageSize, pageNo * pageSize));
+        setCurrentPage(pageNo);
+    }
+    const onPreviousClick = () =>{
+        onchange ( data.slice((currentPage - 2) * pageSize, (currentPage - 1) * pageSize));
+        setCurrentPage(currentPage - 1)
+    }
+    const onNextClick =() =>{
+        onchange(data.slice(currentPage * pageSize, (currentPage +1) * pageSize));
+        setCurrentPage(currentPage+1)
+    }
+    useEffect(()=>{
+        onchange(data.slice(0,5))
+    },[data])
+    return (
+        <div className='pagination-container'>
+            <button disabled={currentPage === 1} onClick={onPreviousClick}>Prev</button>
+            {
+                data.slice(0, totalPages).map((e, index) => {
+                    if (index >= currentPage - 1 && index < currentPage + pageOptions - 1) {
+                        return <div key={index} onClick={() => handleOnChange(index + 1)} className={`pagination-circle ${index + 1 === currentPage && 'pagination-active-page'}`}>{index + 1}</div>
+                    } else {
+                        return <></>
+                    }
+                })
+            }
+            <button disabled={currentPage === totalPages} onClick={onNextClick}>Next</button>
+        </div>
+    )
+}
+
+export default Pagination
